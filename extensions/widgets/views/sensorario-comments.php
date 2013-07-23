849cecaf89f29ae112de0811f760c498921ee581
sensorario <sensorario@gmail.com>
2013-07-23 06:54:59 +0200

    [fixed #20] To leave a comment, you must be logged in

--- extensions/widgets/views/sensorario-comments.php
+++ extensions/widgets/views/sensorario-comments.php
@@ -1,12 +1,8 @@
 <div class="sensorario-comments" id="sensorario-comments-thread-<?php echo $this->thread; ?>">
     <div id="sensorario-comments-stats-<?php echo $this->thread; ?>"></div>
     <div id="sensorario-comments-comments-<?php echo $this->thread; ?>"></div>
-    <?php if (!Yii::app()->user->isGuest) : ?>
-        <div id="sensorario-comments-form-<?php echo $this->thread; ?>">
-            <textarea id="sensorario-comments-textarea-<?php echo $this->thread; ?>"></textarea>
-        </div>
-    <?php else : ?>
-        <em>To leave a comment, you must be logged in.</em>
-    <?php endif; ?>
+    <div id="sensorario-comments-form-<?php echo $this->thread; ?>">
+        <textarea id="sensorario-comments-textarea-<?php echo $this->thread; ?>"></textarea>
+    </div>
 </div>
 
