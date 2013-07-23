791d8c40801d4de1317350302a0212143702ba94
sensorario <sensorario@gmail.com>
2013-07-23 05:26:13 +0200

    [fixed #17] SlideDown on new comments

--- views/ajaxSensorarioComments/_item.php
+++ views/ajaxSensorarioComments/_item.php
@@ -1,4 +1,6 @@
-<div class="sensorario-comments-item" id="sensorario-comment-id-<?php echo $comment->id; ?>">
+<div class="sensorario-comments-item"
+     style="display: none;"
+     id="sensorario-comment-id-<?php echo $comment->id; ?>">
     <div class="sensorario-comments-author">#<?php echo $comment->id; ?> - <?php echo $comment->user; ?></div>
     <div class="sensorario-comments-comment"><?php echo $comment->comment; ?></div>
     <a href="javascript:void(0);" id="sensorario-comments-delete-<?php echo $comment->id; ?>">cancella</a>
@@ -8,6 +10,7 @@ $ajaxLinkController = '/sensorariocomments/ajaxSensorarioComments/';
 $urlDelete = Yii::app()->createUrl($ajaxLinkController . 'delete', array('thread' => $comment->thread))
 ?>
 <script>
+    $('#sensorario-comment-id-<?php echo $comment->id; ?>').slideDown();
     $('#sensorario-comments-delete-<?php echo $comment->id; ?>').on('click', function() {
         $.post('<?php echo $urlDelete; ?>', {
             id: <?php echo $comment->id; ?>
