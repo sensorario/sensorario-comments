4a81c87dacd73c0670faa6cac828ee9d8feb7efd
sensorario <sensorario@gmail.com>
2013-07-23 07:05:36 +0200

    [fixed #15] I cant delete other users commets

--- views/ajaxSensorarioComments/_item.php
+++ views/ajaxSensorarioComments/_item.php
@@ -3,27 +3,31 @@
      id="sensorario-comment-id-<?php echo $comment->id; ?>">
     <div class="sensorario-comments-author">#<?php echo $comment->id; ?> - <?php echo $comment->user; ?></div>
     <div class="sensorario-comments-comment"><?php echo $comment->comment; ?></div>
-    <a href="javascript:void(0);" id="sensorario-comments-delete-<?php echo $comment->id; ?>">delete</a>
+    <?php if ($comment->user === Yii::app()->user->name) : ?>
+        <a href="javascript:void(0);" id="sensorario-comments-delete-<?php echo $comment->id; ?>">delete</a>
+    <?php endif; ?>
 </div>
-<?php
-$ajaxLinkController = '/sensorariocomments/ajaxSensorarioComments/';
-$urlDelete = Yii::app()->createUrl($ajaxLinkController . 'delete', array('thread' => $comment->thread))
-?>
 <script>
     $('#sensorario-comment-id-<?php echo $comment->id; ?>').slideDown();
-    $('#sensorario-comments-delete-<?php echo $comment->id; ?>').on('click', function() {
-        $.post('<?php echo $urlDelete; ?>', {
-            id: <?php echo $comment->id; ?>
-        }, function(json) {
-            if (json.success.toString() === 'false') {
-                alert(json.message);
-            } else {
-                $('#sensorario-comment-id-<?php echo $comment->id; ?>').slideUp('slow', function() {
-                    $(this).remove();
-                });
+<?php if ($comment->user === Yii::app()->user->name) : ?>
+    <?php
+    $ajaxLinkController = '/sensorariocomments/ajaxSensorarioComments/';
+    $urlDelete = Yii::app()->createUrl($ajaxLinkController . 'delete', array('thread' => $comment->thread))
+    ?>
+        $('#sensorario-comments-delete-<?php echo $comment->id; ?>').on('click', function() {
+            $.post('<?php echo $urlDelete; ?>', {
+                id: <?php echo $comment->id; ?>
+            }, function(json) {
+                if (json.success.toString() === 'false') {
+                    alert(json.message);
+                } else {
+                    $('#sensorario-comment-id-<?php echo $comment->id; ?>').slideUp('slow', function() {
+                        $(this).remove();
+                    });
 
-                mostraNumeroCommenti('<?php echo $comment->thread; ?>');
-            }
-        }, 'json');
-    });
+                    mostraNumeroCommenti('<?php echo $comment->thread; ?>');
+                }
+            }, 'json');
+        });
+<?php endif; ?>
 </script>
