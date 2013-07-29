ca4079d2ffec65a1e3da5bcb2e1e9b9c01d45930
sensorario <sensorario@gmail.com>
2013-07-29 05:35:58 +0200

    Fixed StatsAction cs

--- actions/StatsAction.php
+++ actions/StatsAction.php
@@ -1,13 +1,38 @@
 <?php
 
 /**
- * This action show statistics of a single thread.
+ * This is action file for update action
+ * 
+ * PHP version 5
+ *
+ * @category Action
+ * @package  Sensorario\Modules\SensorarioComments\Actions
+ * @author   Simone Gentili <sensorario@gmail.com>
+ * @license  http://opensource.org/licenses/MIT MIT
+ * @version  GIT: 2.1
+ * @link     https://github.com/sensorario/sensorariocomments github repository
+ * 
+ */
+
+
+/**
+ * This is action file for update action
+ * 
+ * @category Action
+ * @package  Sensorario\Modules\SensorarioComments\Actions
+ * @author   Simone Gentili <sensorario@gmail.com>
+ * @license  http://opensource.org/licenses/MIT MIT
+ * @version  Release: 2.1
+ * @link     https://github.com/sensorario/sensorariocomments github repository
+ * 
  */
 class StatsAction extends CAction
 {
 
     /**
      * Run method.
+     * 
+     * @return boolean true
      */
     public function run()
     {
@@ -17,20 +42,24 @@ class StatsAction extends CAction
         $thread = $request->getPost('thread');
 
         $comments = SensorarioCommentsModel::model()
-                ->thread($thread)
-                ->findAll();
+          ->thread($thread)
+          ->findAll();
 
         $totThreadComments = count($comments);
 
-        echo json_encode(array(
-            'request' => $request,
-            'success' => true,
-            'message' => 'Impossibile recuperare le statistiche.',
-            'error' => null,
-            'tot_thread_comments' => $totThreadComments,
-        ));
+        $json = array(
+          'request' => $request,
+          'success' => true,
+          'message' => 'Impossibile recuperare le statistiche.',
+          'error' => null,
+          'tot_thread_comments' => $totThreadComments,
+        );
+
+        echo json_encode($json);
 
         Yii::app()->end();
+
+        return true;
 
     }
 
