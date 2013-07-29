99dab65f2cffd0e91985228ed1976a283c092249
sensorario <sensorario@gmail.com>
2013-07-29 05:37:49 +0200

    Fixed SaveAction cs

--- actions/SaveAction.php
+++ actions/SaveAction.php
@@ -1,13 +1,38 @@
 <?php
 
 /**
- * This action save new comment.
+ * This is action file for update action
+ * 
+ * PHP version 5
+ *
+ * @category Action
+ * @package  Sensorario\Modules\SensorarioComments\Actions
+ * @author   Simone Gentili <sensorario@gmail.com>
+ * @license  http://opensource.org/licenses/MIT MIT
+ * @version  GIT: 2.1
+ * @link     https://github.com/sensorario/sensorariocomments github repository
+ * 
+ */
+
+
+/**
+ * This is action file for update action
+ * 
+ * @category Action
+ * @package  Sensorario\Modules\SensorarioComments\Actions
+ * @author   Simone Gentili <sensorario@gmail.com>
+ * @license  http://opensource.org/licenses/MIT MIT
+ * @version  Release: 2.1
+ * @link     https://github.com/sensorario/sensorariocomments github repository
+ * 
  */
 class SaveAction extends CAction
 {
 
     /**
      * Run method.
+     * 
+     * @return boolean true
      */
     public function run()
     {
@@ -29,18 +54,27 @@ class SaveAction extends CAction
             }
         }
 
-        echo json_encode(array(
-            'post' => $_POST,
-            'get' => $_GET,
-            'success' => $success,
-            'message' => $message,
-            'error' => null,
-            'html' => $this->getController()->renderPartial('_item', array(
-                'isOwner' => Yii::app()->user->name === $sensorarioCommento->user,
-                'comment' => $sensorarioCommento), true),
-        ));
+        $params = array(
+          'isOwner' => Yii::app()->user->name === $sensorarioCommento->user,
+          'comment' => $sensorarioCommento
+        );
+
+        $html = $this->getController()->renderPartial('_item', $params, true);
+
+        $json = array(
+          'post' => $_POST,
+          'get' => $_GET,
+          'success' => $success,
+          'message' => $message,
+          'error' => null,
+          'html' => $html,
+        );
+
+        echo json_encode($json);
 
         Yii::app()->end();
+
+        return true;
 
     }
 
