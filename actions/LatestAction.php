4e43adc02d550e8c4b267fc65e77a4151a8340c9
sensorario <sensorario@gmail.com>
2013-07-29 05:39:20 +0200

    Fixed LatestAction cs

--- actions/LatestAction.php
+++ actions/LatestAction.php
@@ -1,13 +1,38 @@
 <?php
 
 /**
- * This action show latest actions.
+ * This is action file for update action
+ * 
+ * PHP version 5
+ *
+ * @category Action
+ * @package  Sensorario\Modules\SensorarioComments\Actions
+ * @author   Simone Gentili <sensorario@gmail.com>
+ * @license  http://opensource.org/licenses/MIT MIT
+ * @version  GIT: 2.1
+ * @link     https://github.com/sensorario/sensorariocomments github repository
+ * 
+ */
+
+
+/**
+ * This is action file for update action
+ * 
+ * @category Action
+ * @package  Sensorario\Modules\SensorarioComments\Actions
+ * @author   Simone Gentili <sensorario@gmail.com>
+ * @license  http://opensource.org/licenses/MIT MIT
+ * @version  Release: 2.1
+ * @link     https://github.com/sensorario/sensorariocomments github repository
+ * 
  */
 class LatestAction extends CAction
 {
 
     /**
      * Run method.
+     * 
+     * @return boolean true
      */
     public function run()
     {
@@ -16,29 +41,33 @@ class LatestAction extends CAction
         $thread = $request->getPost('thread');
 
         $comments = SensorarioCommentsModel::model()
-                ->thread($thread)
-                ->recenti()
-                ->findAll();
+          ->thread($thread)
+          ->recenti()
+          ->findAll();
 
         $controller = $this->getController();
 
         $html = '';
         foreach ($comments as $comment) {
             $params = array(
-                'comment' => $comment,
-                'isOwner' => Yii::app()->user->name === $comment->user,
+              'comment' => $comment,
+              'isOwner' => Yii::app()->user->name === $comment->user,
             );
             $html = $controller->renderPartial('_item', $params, true) . $html;
         }
 
-        echo json_encode(array(
-            'post' => $_POST,
-            'get' => $_GET,
-            'success' => false,
-            'html' => $html
-        ));
+        $json = array(
+          'post' => $_POST,
+          'get' => $_GET,
+          'success' => false,
+          'html' => $html
+        );
+
+        echo json_encode($json);
 
         Yii::app()->end();
+
+        return true;
 
     }
 
