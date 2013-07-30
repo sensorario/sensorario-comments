fe8e5a5ad523446d83fb5ed0686e47e447f6e79d
sensorario <sensorario@gmail.com>
2013-07-30 02:59:23 +0200

    Added spanish translations

---
+++ messages/es/app.php
@@ -0,0 +1,10 @@
+<?php
+
+return array(
+  'To leave a comment, you must be logged in.' => 'Para dejar un comentario, debes estar registrado',
+  'Message successful updated.' => 'Mensaje actualizado con éxito.',
+  'Statistics unavailable.' => 'No hay estadística disponible.',
+  'Message successful saved.' => 'Mensaje guardado con éxito.',
+  'There are no old messages to show.' => 'No hay mensajes antiguos para mostrar.',
+  'Comment was not deleted.' => 'El comentario no se ha eliminado.',
+);
