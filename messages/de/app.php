f0c219780de776c5e7f0da332e8df1a21b0ab7bb
sensorario <sensorario@gmail.com>
2013-07-30 06:59:41 +0200

    Added german translations

---
+++ messages/de/app.php
@@ -0,0 +1,10 @@
+<?php
+
+return array(
+  'To leave a comment, you must be logged in.' => 'Um einen Kommentar abzugeben, müssen Sie eingeloggt sein.',
+  'Message successful updated.' => 'Nachricht erfolgreich aktualisiert.',
+  'Statistics unavailable.' => 'Statistik nicht verfügbar.',
+  'Message successful saved.' => 'Nachricht erfolgreich gespeichert.',
+  'There are no old messages to show.' => 'Es gibt keine alten Nachrichten zu zeigen.',
+  'Comment was not deleted.' => 'Kommentar wurde nicht gelöscht.',
+);
