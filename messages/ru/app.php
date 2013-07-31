02b20ad577ad69aec57e627235c5d7722bf2204b
sensorario <sensorario@gmail.com>
2013-07-31 06:20:45 +0200

    Added russian translations

---
+++ messages/ru/app.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Translations file.
+ * 
+ * PHP version 5
+ * 
+ * @category Translation
+ * @package  Sensorario\Modules\SensorarioComments\Translation
+ * @author   Simone Gentili <sensorario@gmail.com>
+ * @license  http://opensource.org/licenses/MIT MIT
+ * @version  GIT: 2.1
+ * @link     https://github.com/sensorario/sensorariocomments github repository
+ */
+return array(
+  'To leave a comment, you must be logged in.'
+  => 'Чтобы оставить комментарий, вы должны быть вошли в систему.',
+  'Message successful updated.'
+  => 'Сообщение успешной обновлен.',
+  'Statistics unavailable.'
+  => 'Статистика недоступна.',
+  'Message successful saved.'
+  => 'Сообщение успешной спасен.',
+  'There are no old messages to show.'
+  => 'Там ​​нет старые сообщения, чтобы показать.',
+  'Comment was not deleted.'
+  => 'Il Commento Non è стата cancellato.',
+);
